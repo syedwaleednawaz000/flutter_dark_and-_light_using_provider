d9c02c5f7f0b2939368a8c0869e15f9ebd95e217
Syed Waleed Nawaz <syedwaleednawaz333@gmail.com>
2022-12-03 10:44:14 +0500

    implement all theme light and dark flutter latest version

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import './src/theming/theme_manager.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  return runApp(ChangeNotifierProvider<ThemeNotifier>(
-    create: (_) =>  ThemeNotifier(),
-    child: MyApp(),
-  ));
+  return runApp(
+    MyApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeNotifier()),
+      ],
+      child: Builder(builder: (BuildContext context) {
+        return Consumer<ThemeNotifier>(
+          builder: (key, theme, child) => GetMaterialApp(
+            theme: theme.getTheme(),
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Provider',
+            home: HomePage(),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeNotifier>(
-        builder: (context, theme, child) => MaterialApp(
-              theme: theme.getTheme(),
-              home: Scaffold(
-                backgroundColor: Theme.of(context).focusColor,
-                appBar: AppBar(
-                  ///
-                  ///
-                  ///
-                  title: Text('Hybrid Theme'),
-                ),
-                body: Row(
-                  children: [
-                    Container(
-                      child: TextButton(
-                        onPressed: () => {
-                          print('Set Light Theme'),
-                          theme.setLightMode(),
-                        },
-                        child: Text('Set Light Theme'),
+        builder: (context, theme, child) => Scaffold(
+              backgroundColor: Theme.of(context).focusColor,
+              appBar: AppBar(
+                title: Text('Hybrid Theme'),
+              ),
+              body: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        child: TextButton(
+                          onPressed: () => {
+                            print('Set Light Theme'),
+                            theme.setLightMode(),
+                          },
+                          child: Text('Set Light Theme'),
+                        ),
                       ),
-                    ),
-                    Container(
-                      child: TextButton(
-                        onPressed: () => {
-                          print('Set Dark theme'),
-                          theme.setDarkMode(),
-                        },
-                        child: Text('Set Dark theme'),
+                      Container(
+                        child: TextButton(
+                          onPressed: () => {
+                            print('Set Dark theme'),
+                            theme.setDarkMode(),
+                          },
+                          child: Text('Set Dark theme'),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'this is waleed nawaz',
+                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  )
+                ],
               ),
             ));
   }
